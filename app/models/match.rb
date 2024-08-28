efdd8480c24e0772c4e611a1ef1b62601786f8d6
teresa amorim <te.botelho.amorim@gmail.com>
2024-08-28 22:38:36 +0100

    MatchesCardsPage

--- app/models/match.rb
+++ app/models/match.rb
@@ -3,4 +3,16 @@ class Match < ApplicationRecord
   belongs_to :sport
   has_many :UserMatches
   has_many :users, through: :UserMatches
+
+  ICONS = {
+    "volleyball" => "fa-solid fa-volleyball",
+    "football" => "fa-regular fa-futbol",
+    "basketball" => "fa-solid fa-basketball",
+    "padel" => "fa-solid fa-table-tennis-paddle-ball",
+    "squash" => "fa-solid fa-table-tennis-paddle-ball"
+  }.freeze
+
+  def sport_icon
+    ICONS.fetch(sport.name, "fa-solid fa-question")
+  end
 end
