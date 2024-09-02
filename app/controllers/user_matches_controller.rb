cac4edadc9355453e705718df909cb7d0d68b06f
Zainan Ali <zainanzaher09@gmail.com>
2024-09-02 12:12:17 +0100

    cd

--- app/controllers/user_matches_controller.rb
+++ app/controllers/user_matches_controller.rb
@@ -19,6 +19,8 @@ class UserMatchesController < ApplicationController
     end
   end
 
+
+
   def cancel_match
     if current_user == @usermatch.user
       @usermatch.destroy
@@ -39,6 +41,7 @@ class UserMatchesController < ApplicationController
   def update_user_request_status(usermatch, status)
     if current_user == usermatch.match.user
       if status == "accept" ? usermatch.accepted! : usermatch.denied!
+        set_user_team(usermatch) if status == "accept"
         render json: { message: usermatch }
       else
         render json: { error: "Something went wrong!" }
@@ -48,8 +51,21 @@ class UserMatchesController < ApplicationController
     end
   end
 
+  def set_user_team(usermatch)
+    total_players = usermatch.match.sport.number_of_players.to_i # getting total players need in a sport
+    players_needed = total_players - usermatch.match.need.to_i # gets players we need
+    total_players_each_team = (total_players.to_f % 2).zero? ? total_players / 2 : [(total_players.to_f / 2).floor, (total_players.to_f / 2).ceil ] # if half of total players is even then it returns a number otherwise an array [lowernum, highernum]
+    players_have = usermatch.match.need # player that already exists.
+    each_team = (players_have.to_f % 2).zero? ? players_have / 2 : [(players_have.to_f / 2).floor, (players_have.to_f / 2).ceil ] # checks for existing matches
+    team_a = (total_players_each_team.is_a?(Array) ? total_players_each_team[0] : total_players_each_team) - (each_team.is_a?(Array) ? each_team[0] : each_team)
+    team_b = (total_players_each_team.is_a?(Array) ? total_players_each_team[1] : total_players_each_team) - (each_team.is_a?(Array) ? each_team[1] : each_team)
+    current_team_a = usermatch.match.UserMatches.where(team: "teamA").count || 0
+    current_team_b = usermatch.match.UserMatches.where(team: "teamB").count || 0
+    team_a - current_team_a > team_b - current_team_b ? usermatch.teamA! : usermatch.teamB!
+  end
+
   def set_usermatch
-    @usermatch = UserMatch.find(params[:id]);
+    @usermatch = UserMatch.find(params[:id])
   end
 
   def already_requested(match)
