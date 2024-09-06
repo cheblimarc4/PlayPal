7f285c9d98759809c1dc2a5eadf25f0ebd97e386
Marc Chebli <cheblimarc4@gmail.com>
2024-09-06 16:19:18 +0100

    fixed seeD

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,10 +6,11 @@ class PagesController < ApplicationController
   end
 
   def myprofile
-    @matches = current_user.matches.where(ready: true)
+    @um = UserMatch.where(user: current_user)
+    @user_matches = @um.where(status: "accepted")
+    @matches = @user_matches.map { |usermatch| usermatch.match }
     @total_wins, @total_losses, @total_matches = calculate_wins(@matches)
     @user = current_user
-    @user_matches = UserMatch.where(user: current_user)
   end
 
   def calculate_wins(matches)
@@ -17,12 +18,12 @@ class PagesController < ApplicationController
     wins = 0.0
     total_matches = 0.0
     matches.each do |match|
-      total_matches += 1
       next if match.winning_team.nil?
+      total_matches += 1.0
 
-      winning_team = match.winning_team == 1 ? "teamA" : "teamB"
+      winning_team = match.winning_team == 1.0 ? "teamA" : "teamB"
       usermatch = match.UserMatches.find_by(user: current_user)
-      usermatch.team == winning_team ? wins += 1 : losses += 1
+      usermatch.team == winning_team ? wins += 1.0 : losses += 1.0
     end
 
     return wins, losses, total_matches
--- db/seeds.rb
+++ db/seeds.rb
@@ -29,7 +29,7 @@ levels = ["Beginner", "Intermediate", "Advanced"]
 types = ["Friendly", "Competitive"]
 
 
-names = ["Margarida", "João", "David", "Ines", "Rita", "Henrique", "Marta Rosa", "Mariana", "Francisco P1", "Francisco P2", "Gonçalo","Marc", "Teresa", "Guillaume", "Lucelia", "Zainan", "Afonso"]
+names = ["Margarida", "João", "David", "Ines", "Rita", "Henrique", "Marta Rosa", "Mariana", "Francisco", "Francisco Teacher", "Gonçalo","Marc", "Teresa", "Guillaume", "Lucelia", "Zainan", "Afonso"]
 photo = ["https://ca.slack-edge.com/T02NE0241-USN1K7YLE-ce4c21c34c01-512","https://ca.slack-edge.com/T02NE0241-U8A945YB0-5a961e901c6a-512", "https://ca.slack-edge.com/T02NE0241-U01439WBLBA-03c468fd4087-512", "https://ca.slack-edge.com/T02NE0241-U028GBBK6MR-a12d5f81d0ad-512", "https://ca.slack-edge.com/T02NE0241-UHU7RTVL4-d16238381bd6-512", "https://ca.slack-edge.com/T02NE0241-U028GBY5DFC-ed25edc41a04-512", "https://ca.slack-edge.com/T02NE0241-U078RSC24G1-3fe4d728e522-512","https://ca.slack-edge.com/T02NE0241-U8RK8CLFN-bcfaccdf0495-512","https://ca.slack-edge.com/T02NE0241-U02TRJ4F37B-826d1eb9bef6-512","https://ca.slack-edge.com/T02NE0241-U058ZAEHAK0-3c9186eb7b4f-512","https://ca.slack-edge.com/T02NE0241-U07B52UHMJS-d9cfa1feec0f-512","https://ca.slack-edge.com/T02NE0241-U07C0U8Q4D6-58a0dcba64fc-512","https://ca.slack-edge.com/T02NE0241-U07AXMZKK9D-20d05d5991f2-512","https://ca.slack-edge.com/T02NE0241-U07BAF5P750-c50eceafbba1-512","https://ca.slack-edge.com/T02NE0241-U075Z0K9KD4-b2b4659d1075-512","https://ca.slack-edge.com/T02NE0241-U07BZJ2RNHE-c61614479c7f-72","https://ca.slack-edge.com/T02NE0241-U07B9TSJG1Z-83002c8afa7e-512"]
 ages = [25, 30, 35, 40, 45, 50, 55, 60, 65, 70]
 emails = names.map { |name| name.downcase.delete(" ") + "@gmail.com" }
@@ -75,13 +75,16 @@ puts "Creating David's football match"
 
 matches = []
 UserMatch.where(user: User.find_by(first_name: "David")).each do |usermatch|
+  next if usermatch.match.sport.name == "squash"
   usermatch.update!(status: "accepted")
-  usermatch.update!(team: (Random.rand() > 0.5 ? "teamA" : "teamB"))
+  unless usermatch.match.user == usermatch.user
+    usermatch.update!(team: (Random.rand() > 0.5 ? "teamA" : "teamB"))
+  end
   puts "#{usermatch}'s match found"
   total_players = usermatch.match.sport.number_of_players
   puts "#{total_players} players needed"
-  players_in_match = usermatch.match.need + usermatch.match.UserMatches.where(status: "accepted").count
-  puts "#{players_in_match} players in match"
+  players_in_match = usermatch.match.need + usermatch.match.UserMatches.where(team: "teamA").count + usermatch.match.UserMatches.where(team: "teamB").count
+  puts "#{players_in_match} for #{usermatch.match.sport.name}"
   players_needed = total_players - players_in_match
   puts "#{players_needed} players needed"
   players_needed.times do
@@ -105,8 +108,16 @@ UserMatch.where(user: User.find_by(first_name: "David")).each do |usermatch|
 end
 puts "Setting matches as ready and rating"
 matches.each do |match|
-  match.update(ready: true)
-  match.update(team_a_score: rand(0..10), team_b_score: rand(0..10), winning_team: rand(1..2))
+  match.update!(ready: true)
+  a_score = rand(0..50)
+  b_score = rand(0..50)
+  while a_score == b_score
+    b_score = rand(0..50)
+  end
+  puts a_score
+  puts b_score
+  winner = (a_score > b_score ? 1 : 2)
+  match.update!(team_a_score: a_score, team_b_score: b_score, winning_team: winner)
 end
 
 
